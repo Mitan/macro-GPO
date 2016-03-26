d80e00c0c1ab250568906eafb91204d7bdda9d7b
unknown <a0134673@soccf-air3-010.stu.nus.edu.sg>
2016-03-26 21:43:49 +0800

    add processing of non-square dataset

--- ChunKai/main_testing_script.py
+++ ChunKai/main_testing_script.py
@@ -1,23 +1,31 @@
 import numpy as np
 import sys
+from HyperInference import __Cosines
 from DatasetInfo import DropWaveInfo, AckleyInfo, CosinesInfo, BraninInfo, GriewankInfo, McCormickInfo, \
-    SixCamelInfo, HolderTableInfo
+    SixCamelInfo, HolderTableInfo, __DatasetInfo
 from TreePlanTester import TestScenario
 
 
 # Random initial locations
 def GenerateInitialLocation(current_f, batch_size):
-    # todo
-    # note: hardcoded
-    default_grid_size_x = 20
-    default_grid_size_y = 20
 
     domain = current_f.domain
     gap = current_f.grid_gap
-    min_x = domain[0][0]
-    min_y = domain[1][0]
-    int_x = np.random.random_integers(low=0, high=default_grid_size_x, size=batch_size)
-    int_y = np.random.random_integers(low=0, high=default_grid_size_y, size=batch_size)
+    min_x, max_x = domain[0]
+    min_y, max_y = domain[1]
+
+    # number of samples at each coordinate
+    default_grid_size_x = float(max_x - min_x) / gap
+    default_grid_size_y = float(max_y - min_y) / gap
+
+    #todo
+    # should add some check for this
+    default_grid_size_x = int(default_grid_size_x)
+    default_grid_size_y = int(default_grid_size_y)
+
+    # in current implementation higher limit is not included hence -1
+    int_x = np.random.random_integers(low=0, high=default_grid_size_x - 1, size=batch_size)
+    int_y = np.random.random_integers(low=0, high=default_grid_size_y - 1, size=batch_size)
 
     # coordinates of location by x and by y
     int_x = min_x + gap * int_x
@@ -50,7 +58,7 @@ def GetSimulatedFunction(i):
         return HolderTableInfo()
 
 if __name__ == '__main__':
-
+    
     args = sys.argv
     batch_size = 2
     save_trunk = "./tests/"
@@ -73,3 +81,10 @@ if __name__ == '__main__':
 
     TestScenario(b=batch_size, beta=beta, location=initial_location, i = location_iteration,  simulated_func=current_function,
                  save_trunk=save_trunk)
+    """
+    test_f = __DatasetInfo(f=__Cosines, lengthscale=(0.12605123651, 0.126051232038),
+                                   signal_variance=0.0198660061591, noise_variance=0.0001, mean=0.940527042428,
+                                   domain=((0.0, 4.0), (0.0, 1.0)), grid_gap=0.05)
+    for i in range(50):
+        print GenerateInitialLocation(test_f,2)
+    """
