fcd179fb9e3bb1bc10416f18fc3c7ab75cf850b6
unknown <a0134673@soccf-air3-010.stu.nus.edu.sg>
2016-04-01 19:33:20 +0800

    fix lengthscale bug

--- ChunKai/GaussianProcess.py
+++ ChunKai/GaussianProcess.py
@@ -240,7 +240,8 @@ class SquareExponential(CovarianceFunction):
         @param: length_scale l - array or list containing the length scales for each dimension
         @param: signal variance sigma_f_squared - float containing the signal variance
         """
-
+        # lenthscales should be squared
+        length_scale = map(lambda x: x**2, length_scale)
         self.length_scale = np.atleast_2d(length_scale)
         self.signal_variance = signal_variance
 
