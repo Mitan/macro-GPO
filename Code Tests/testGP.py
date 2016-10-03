30974576d8042705509afd43bfdd8824458db57b
Mitan <dmitrii@comp.nus.edu.sg>
2016-10-03 17:09:56 +0800

    change GP comparison test

--- Code Tests/testGP.py
+++ Code Tests/testGP.py
@@ -7,20 +7,24 @@ from src.GaussianProcess import GaussianProcess, SquareExponential
 
 __author__ = 'a0134673'
 
-lengthscale = [2.0, 3.0]
-signal = 43.3
-noise = 2.11
-
 
 def f(x):
     return math.sqrt(2 * x[0]**2 + 23 * x[1]**2)
-
+"""
 X = np.arange(-2.0, 2.0, step= 0.2)
 Y = np.arange(-2.0, 2.0, step= 0.2)
 grid = np.asarray([[x0, y0] for x0 in X for y0 in Y])
 values = np.apply_along_axis(f, 1, grid)
 values = values.reshape((values.shape[0], 1))
 
+
+X = np.random.uniform(-3.,3.,(20,2))
+Y = np.sin(X) + np.random.randn(20,2)*0.1
+
+kernel = GPy.kern.RBF(input_dim=2,variance= signal,lengthscale=lengthscale, ARD= True)
+m = GPy.models.GPRegression(X,Y,kernel, noise_var=noise)
+m.optimize_restarts(num_restarts = 10)
+"""
 
 def GPY_predict(test_point):
     kernel = GPy.kern.RBF(input_dim=2,variance= signal,lengthscale=lengthscale, ARD= True)
@@ -47,29 +51,53 @@ def MyPredictiSinglePoint(test_point):
     return mean, var
 #test_point = np.asarray([[1.5, 2.3], [3.4, 2.7]])
 
-for i in range(100):
-    test_point =  np.random.rand(1,2)
-    #print test_point
-    mu_chun, sigma_chun = MyPredictiSinglePoint(test_point)
-    mu_gp, sigma_gp = GPY_predict(test_point)
-    print mu_chun[0,0] / mu_gp[0,0], sigma_chun[0,0] / sigma_gp[0,0]
-    #print mu_gp, sigma_gp
-    """
-    mu_chun, sigma_chun = ChunKai_predict(test_point)
-    mu_gp, sigma_gp = GPY_predict(test_point)
 
-    print mu_gp, sigma_gp
-    mu_diff = np.linalg.norm(mu_chun - mu_gp)
-    sigma_diff = np.linalg.norm(sigma_chun - sigma_gp)
-    print
-    print mu_chun, sigma_chun
-    #print sigma_diff
+for k in range(10):
+    dim = 3
+    num_points = 10
+    X = np.random.uniform(-3., 3., (num_points, dim))
+    X = X - np.mean(X, axis=0)
 
-    if mu_diff > 0.001:
-        print "mu"
-        print mu_diff
+    Y = np.apply_along_axis(f, 1, X).reshape((num_points,1)) + np.random.randn(num_points, 1) * 0.2
+    #Y = np.sin(X) + np.random.randn(10, dim) * 0.2
+    #print Y
+    length_scale = [1.0] * dim
 
-    if sigma_diff  > 0.001:
-        print "sigma"
-        print sigma_diff
-    """
+    diff_chun = 0
+    diff_gpy = 0
+
+    for i in range(100):
+        test_point = np.random.rand(1, dim)
+        test_value = np.sin(test_point) + np.random.rand(1, dim) * 0.2
+        # kernel = GPy.kern.RBF(input_dim=1, variance=1.0, lengthscale=[1.0, 1.0], ARD=True)
+        kernel = GPy.kern.RBF(input_dim=dim, variance=1.0, lengthscale=length_scale, ARD=True)
+
+        m = GPy.models.GPRegression(X, Y, kernel, noise_var=0.01)
+        # print m
+        m.optimize(messages=False)
+        # print m
+        mu_gpy = m.predict(test_point)[0]
+        # params
+        signal =  m.param_array[0]
+        length_scale = m.param_array[1:dim+1]
+        noise = m.param_array[dim+1]
+        # print length_scale
+        """
+        covariance_function = SquareExponential(np.array(length_scale), signal)
+        gp = GaussianProcess(covariance_function, noise, mean_function=0.0)
+        chol = gp.GPCholTraining(X)
+        weights = gp.GPBatchWeights(X, test_point, chol)
+        mu_chun = gp.GPBatchMean(Y, weights)
+        """
+        covariance_function = SquareExponential(np.array(length_scale), signal)
+        gp = GaussianProcess(covariance_function, noise, mean_function=0.0)
+        mu_chun = gp.GPMean(X, Y, test_point)
+
+        diff_chun += np.linalg.norm(mu_chun - test_value)**2
+        diff_gpy += np.linalg.norm(mu_gpy - test_value)**2
+
+
+    #   print "Chun Kai {}".format( math.sqrt(diff_chun))
+    #   print "GPY {}" .format(math.sqrt(diff_gpy))
+    best = "Chun" if diff_chun < diff_gpy else "GPy"
+    print best
