e5f70fdff27a6d345fc7ac601391053191f77dc8
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-01 00:56:31 +0800

    testing of hypers

--- Code Tests/testScript.py
+++ Code Tests/testScript.py
@@ -172,8 +172,13 @@ print a[0]
 a[0] = 44
 print a
 """
-
+"""
 import scipy.io
 mat = scipy.io.loadmat('../src/taxi44.mat')['taxi']
 b =  mat[0][0]
 print b[4]
+"""
+
+a = np.array([[1,2,0 ], [3,4,5], [6,7,8]])
+b = a[[0,1], :]
+print b
--- src/hypers.py
+++ src/hypers.py
@@ -3,12 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from numpy.random import normal
+from random import choice
+from math import sqrt
 
-def TestPrediction():
-    # choice 10
-    # 10 to learn 10 to test
-    pass
 
+def TestPrediction(locs, vals):
+    test_num_point = 20
+    signal = 1.0
+    noise = 0.1
+    mu = 0.0
+    l_1 = 1.0
+    l_2 = 1.0
+
+    vals = vals - mu
+
+    number_of_points = locs.shape[0]
+    array_range = np.arange(number_of_points)
+    indexes = np.random.choice(array_range, test_num_point).tolist()
+    train = indexes[:test_num_point / 2]
+    test = indexes[test_num_point / 2:]
+    locs_train = locs[train, :]
+    vals_train = vals[train, :]
+
+    kernel = GPy.kern.RBF(input_dim=2, variance=signal, lengthscale=[l_1, l_2], ARD=True)
+    m = GPy.models.GPRegression(locs_train, vals_train, kernel, noise_var=noise, normalizer=False)
+    error = 0
+    for test_p in test:
+        predict =  m.predict(locs[test_p, :], full_cov=True) + mu
+        truth = vals[test_p]
+        error += (predict - truth)**2
+
+    print sqrt(error/ (test_num_point / 2))
 
 
 # required to provide initial guess for hypers
@@ -16,41 +41,46 @@ def InferHypers(X, Y, noise, signal, l_1, l_2):
     mu = np.mean(Y)
     Y = Y - mu
     assert X.shape[0] == Y.shape[0]
-    assert X.shape[1] ==2
+    assert X.shape[1] == 2
 
-    kernel = GPy.kern.RBF(input_dim=2,variance= signal,lengthscale=[l_1, l_2], ARD= True)
-    m = GPy.models.GPRegression(X,Y,kernel, noise_var=noise)
-    #m.constrain_bounded('rbf_var',1e-3,1e5)
-    #m.constrain_bounded('rbf_len',.1,200.)
-    #m.constrain_fixed('noise',1e-5)
+    kernel = GPy.kern.RBF(input_dim=2, variance=signal, lengthscale=[l_1, l_2], ARD=True)
+    m = GPy.models.GPRegression(X, Y, kernel, noise_var=noise)
+    # m.constrain_bounded('rbf_var',1e-3,1e5)
+    # m.constrain_bounded('rbf_len',.1,200.)
+    # m.constrain_fixed('noise',1e-5)
 
-    #print m
-    #print m
+    # print m
+    # print m
 
-    #m.optimize(messages=True)
+    # m.optimize(messages=True)
     m.optimize(messages=False)
-    m.optimize_restarts(num_restarts = 10)
-
+    m.optimize_restarts(num_restarts=10)
 
     # lengthscales go indexes 1 and 2
-    #todo note need to square the l_1 and l_2
-    l_1, l_2 =  m.param_array[1:3]
-    #print l_1, l_2
+    # todo note need to square the l_1 and l_2
+    l_1, l_2 = m.param_array[1:3]
+    # print l_1, l_2
 
     # todo note this is already sigma^2
     noise_variance = m.param_array[3]
-    #print noise_variance
+    # print noise_variance
 
     signal_variance = m.param_array[0]
-    #print signal_variance
-    #print m
-    #return l_1, l_2, noise_variance, signal_variance
+    # print signal_variance
+    # print m
+    # return l_1, l_2, noise_variance, signal_variance
     print m
-    return mu, l_1, l_2, noise_variance,signal_variance
+    """
+    out_file = open('hypers_learnt.txt', 'w')
+    out_file.write(str(mu) + ' ' + str(l_1) + ' ' + str(l_2) + ' ' + str(noise_variance) + ' ' + str(signal_variance))
+    out_file.close()
+    """
+    return mu, l_1, l_2, noise_variance, signal_variance
     # return m, mu
 
+
 if __name__ == "__main__":
-    filename = './hypers44.txt'
+    filename = './hypers18.txt'
     data = np.genfromtxt(filename)
     # print data
     locs = data[:, :2]
@@ -67,22 +97,19 @@ if __name__ == "__main__":
     vals = np.add(vals, normals)
     print scipy.stats.skew(vals)
 
-    vals = np.log(vals+ 0.01)
-
+    vals = np.log(vals + 0.1)
 
     print scipy.stats.skew(vals)
 
-
     plt.hist(vals, bins=100)
 
-    plt.show()
+    # plt.show()
 
-    """
+
     vals = np.atleast_2d(vals).T
 
-    print locs.shape, vals.shape
-
-    signal = max(vals) - min(vals)
+    signal = (max(vals) - min(vals)) / 2
+    l_1 = 2.0 / 25.0
+    l_2 = 2.0 / 50.0
     noise = 0.1 * signal
-    print InferHypers(locs,vals, noise, signal, 5.0, 10.0)
-    """
+    print InferHypers(X=locs, Y=vals, noise=noise, signal=signal, l_1=l_1, l_2=l_2)
