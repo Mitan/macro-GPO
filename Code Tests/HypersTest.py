5a3f79b38091caba3aa51a434c103d6165f1fce9
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-06 23:06:09 +0800

    add hypers test

--- Code Tests/HypersTest.py
+++ Code Tests/HypersTest.py
@@ -1,29 +1,35 @@
+import random
+
 from src.DatasetUtils import GenerateRoadModelFromFile
 from src.GaussianProcess import SquareExponential, GaussianProcess
 from src.HypersStorer import RoadHypersStorer_Log44
 import numpy as np
 from math import sqrt
 
+from src.Utils import GenerateGridPairs, TupleToLine
+
 
 def TestPrediction(locs, vals):
 
-    test_num_point = 20
+    train_number_of_points = 10
+    test_number_of_points = 5
+
 
     hypers = RoadHypersStorer_Log44()
 
     mu = hypers.mean_function
-    vals = vals - mu
 
     # total number of points
     number_of_points = locs.shape[0]
-    array_range = np.arange(number_of_points)
 
     # choose a list of points from dataset
-    indexes = np.random.choice(array_range, test_num_point).tolist()
+    indexes = random.sample(xrange(number_of_points), train_number_of_points+ test_number_of_points)
 
-    half_points = test_num_point / 2
-    train = indexes[:half_points]
-    test = indexes[half_points:]
+    # those are INDEXES
+    train = indexes[:train_number_of_points]
+    test = indexes[train_number_of_points:]
+    print train, test
+
     # 2d array
     locs_train = locs[train, :]
     # print locs_train
@@ -49,10 +55,10 @@ def TestPrediction(locs, vals):
         mu_predict = gp.GPMean(measurements=vals_train, weights=weights)
 
         truth = vals[test_p]
-        # print truth, mu_predict
+        print point, truth, mu_predict
         error += (mu_predict- truth)**2
 
-    print sqrt(error / (half_points))
+    print sqrt(error / (test_number_of_points))
 
 
 if __name__ == "__main__":
@@ -64,6 +70,11 @@ if __name__ == "__main__":
 
     #TestPrediction(locs, vals)
 
-    g = np.meshgrid(range(10, 12), range(110,114))
-    pairs = np.append(g[0].reshape(-1, 1), g[1].reshape(-1, 1), axis=1)
-    print pairs
+    pairs = GenerateGridPairs(range(8,12), range(40,44)).tolist()
+
+    raw_indexes = map(lambda x: TupleToLine(x, m.dim_1, m.dim_2), pairs)
+    indexes = [x for x in raw_indexes if vals[x]!= m.NO_DATA_CONST]
+    selected_locs = locs[indexes, :]
+    print selected_locs
+    selected_vals = vals[indexes]
+    TestPrediction(selected_locs, selected_vals)
