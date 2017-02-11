d571203c06f0e5e0bcec9531b8a3f9e3ab223d7b
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-12 00:45:06 +0800

    minor

--- src/betaTest.py
+++ src/betaTest.py
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     seed_0 = int(args[1])
     time_slot = int(args[2])
 
-    seed_0, time_slot = (0, 18)
+    # seed_0, time_slot = (0, 18)
 
     t, batch_size, num_samples = (4, 5, 250)
 
     filename = '../datasets/slot' + str(time_slot) + '/tlog' + str(time_slot) + '.dom'
 
-    my_save_folder_root = "../testsRoadBeta/b" + str(batch_size) + "/" + str(time_slot) + "/"
+    my_save_folder_root = "../testsRoadBeta2/b" + str(batch_size) + "/" + str(time_slot) + "/"
 
     for seed in range(seed_0, seed_0 + 2):
         TestScenario_Beta(my_save_folder_root=my_save_folder_root, test_horizon=h, seed=seed, time_steps=t,
