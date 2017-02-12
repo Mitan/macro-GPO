328534dc30cd4443f08e8ed846211465d50a796e
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-12 20:31:54 +0800

    minor

--- src/h4_script.py
+++ src/h4_script.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     filename = '../datasets/slot' + str(time_slot) + '/tlog' + str(time_slot) + '.dom'
 
     my_save_folder_root = "../testsRoad2/b" + str(batch_size) + "/" + str(time_slot) + "/"
-    my_save_folder_root = "../testsRoad/"
+    my_save_folder_root = "../testsRoad_4/"
 
-    for seed in range(seed_0, seed_0 + 1):
+    for seed in range(seed_0, seed_0 + 3):
         TestScenario_H4(my_save_folder_root=my_save_folder_root, seed=seed, time_steps=t, num_samples=num_samples,
                         batch_size=batch_size, filename=filename, time_slot=time_slot)
