ba89a8fef2dbdecd8bd03478704e34c30f9dc332
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-12 13:32:23 +0800

    H=4 write rewards to file

--- src/ResultsAveragerBeta.py
+++ src/ResultsAveragerBeta.py
@@ -3,17 +3,19 @@ import numpy as np
 from StringIO import StringIO
 
 
-seeds = range(66,102)
+seeds = range(32)
+seeds = list(set(seeds) - set([1]))
 
-root_path = '../testsBeta3/'
+root_path = '../testsRoadBeta2/b5/18/'
 # root_path = '../testBeta2/'
 
 
 beta_list = ['1e-05', '1e-06', '1e-07',10**-4, 10**-3, 5* 10**-3, 10**-2, 5* 10**-2,0.0, 0.1, 1.0, 2.0, 5.0, 10.0, 50.0, 100.0, 200.0]
-beta_list = [10**-3,0.0, 0.1, 1.0, 2.0, 10.0]
+# beta_list = [10**-3,0.0, 0.1, 1.0, 2.0, 10.0]
+beta_list = [10 ** -7, 10 ** -6, 10 ** -5, 10 ** -4, 10 ** -3, 5 * 10 ** -3, 10 ** -2, 5 * 10 ** -2, 0.0, 0.1, 1.0,
+             2.0, 5.0, 10.0, 50.0, 100.0, 200.0]
 
-
-steps = 5
+steps = 4
 
 results = []
 
@@ -30,6 +32,7 @@ for beta in beta_list:
             # print a
             number_of_location += 1
         except:
+            print file_path
             continue
 
         a = StringIO(a)
--- src/TestScenario.py
+++ src/TestScenario.py
@@ -12,6 +12,7 @@ from HypersStorer import SimulatedHyperStorer
 
 
 def TestScenario_H4(my_save_folder_root, seed, time_steps, num_samples, batch_size, time_slot, filename):
+
     save_folder = my_save_folder_root + "seed" + str(seed) + "/"
 
     try:
@@ -27,11 +28,25 @@ def TestScenario_H4(my_save_folder_root, seed, time_steps, num_samples, batch_si
 
     h = 4
 
-    testWithFixedParameters(time_slot=time_slot, model=m, method=Methods.Anytime, horizon=h,
+    filename_rewards = save_folder + "reward_histories.txt"
+    if os.path.exists(filename_rewards):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+
+    output_rewards = open(filename_rewards, append_write)
+
+    h4 = testWithFixedParameters(time_slot=time_slot, model=m, method=Methods.Anytime, horizon=h,
                             num_timesteps_test=time_steps,
                             save_folder=save_folder + "anytime_h" + str(h) + "/",
                             num_samples=num_samples, batch_size=batch_size,
                             start_location=start_location)
+
+    method_name = 'Anytime H = ' + str(h)
+
+    output_rewards.write(method_name + '\n')
+    output_rewards.write(str(h4) + '\n')
+    output_rewards.close()
 
 
 def TestScenario(my_save_folder_root, h_max, seed, time_steps, num_samples, batch_size, time_slot, filename=None):
@@ -46,9 +61,16 @@ def TestScenario(my_save_folder_root, h_max, seed, time_steps, num_samples, batc
         if not os.path.isdir(save_folder):
             raise
 
+    filename_rewards = save_folder + "reward_histories.txt"
+    if os.path.exists(filename_rewards):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+
     # file for storing reward histories
     # so that later we can plot only some of them
-    output_rewards = open(save_folder + "reward_histories.txt", 'w')
+
+    output_rewards = open(filename_rewards, append_write)
 
     # Generation of simulated model
     """
@@ -165,6 +187,7 @@ def TestScenario(my_save_folder_root, h_max, seed, time_steps, num_samples, batc
         output_rewards.write(method_name + '\n')
         output_rewards.write(str(current_h_result) + '\n')
 
+    output_rewards.close()
     PlotData(result_graphs, save_folder)
 
 
@@ -182,6 +205,8 @@ def TestScenario_Beta(my_save_folder_root, seed, time_steps, num_samples, batch_
     except OSError:
         if not os.path.isdir(save_folder):
             raise
+
+
 
     output_rewards = open(save_folder + "reward_histories.txt", 'w')
 
