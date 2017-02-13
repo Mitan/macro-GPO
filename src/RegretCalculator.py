8dfbaa72c5de2dda3ebdf5f242803b3899f03c5b
Mitan <dmitrii@comp.nus.edu.sg>
2017-02-13 15:46:39 +0800

    regret calculation

--- src/RegretCalculator.py
+++ src/RegretCalculator.py
@@ -1,15 +1,50 @@
 from StringIO import StringIO
 import numpy as np
 
+from src.DatasetUtils import GenerateRoadModelFromFile
+from src.ResultsPlotter import PlotData
 
-def CalculateMethodRegret(model_max, root_folder, method_name, batch_size):
 
+def CalculateAverageRegret(model_max, root_path, seeds, methods, method_names, batch_size):
+    # seeds = range(36)
+
+    len_seeds = len(seeds)
+
+    # methods = ['h1', 'anytime_h2', 'anytime_h3','mle_h3', 'qEI' ]
+    # method_names = ['Myopic UCB', 'Anytime H = 2', 'Anytime H = 3','MLE H = 3', 'qEI']
+
+    # root_path = '../testsRoad/b' + str(batch_size) + '/'+ str(slot) + '/'
+
+    steps = 20 / batch_size
+
+    results = []
+
+    for index, method in enumerate(methods):
+
+        results_for_method = np.zeros((steps,))
+
+        for seed in seeds:
+            seed_folder = root_path + 'seed' + str(seed) + '/'
+            max_found_values = CalculateMethodMaxValues(seed_folder, method, batch_size)
+            assert max_found_values is not None
+            results_for_method = np.add(results_for_method, max_found_values)
+
+        results_for_method = results_for_method / len_seeds
+        regrets = [model_max - res for res in results_for_method.tolist()]
+        result = [method_names[index], regrets]
+        results.append(result)
+        print result
+
+        # PlotData(results, root_path)
+
+
+def CalculateMethodMaxValues(root_folder, method_name, batch_size):
     n_steps = 20 / batch_size
-    regrets = []
+    max_found_values = []
     for i in range(n_steps):
         step_file_name = root_folder + method_name + '/step' + str(i) + '.txt'
         lines = open(step_file_name).readlines()
-        first_line_index = 1 + batch_size + 1 + (1 + batch_size * (i+1)) + 1
+        first_line_index = 1 + batch_size + 1 + (1 + batch_size * (i + 1)) + 1
         last_line_index = -1
         stripped_lines = map(lambda x: x.strip(), lines[first_line_index: last_line_index])
         joined_lines = " ".join(stripped_lines)
@@ -20,19 +55,25 @@ def CalculateMethodRegret(model_max, root_folder, method_name, batch_size):
         # all measurements obtained by the robot till that step
         measurements = np.genfromtxt(a)
 
-        assert measurements.shape[0] == 1 + batch_size * (i +1)
+        assert measurements.shape[0] == 1 + batch_size * (i + 1)
         # assert we parsed them all as numbers
         assert not np.isnan(measurements).any()
 
         max_found = max(measurements)
-        regrets.append(model_max - max_found)
-    return regrets
+        max_found_values.append(max_found)
+    return max_found_values
 
 
 if __name__ == "__main__":
     # cannot use - cylcic linking
-    folder_name = '../testsRoad/b5/18/seed0/'
+    folder_name = '../testsRoad/b5/18/'
+    seeds = range(20)
     b = 5
-    max_model = 0
-    method  = 'anytime_h2'
-    print CalculateMethodRegret(max_model, folder_name,method, b)
+    methods = ['h1', 'anytime_h2', 'anytime_h3', 'mle_h3', 'qEI']
+    method_names = ['Myopic UCB', 'Anytime H = 2', 'Anytime H = 3', 'MLE H = 3', 'qEI']
+
+    file_name = '../datasets/slot18/tlog18.dom'
+    m = GenerateRoadModelFromFile(file_name)
+    model_max = m.GetMax()
+    CalculateAverageRegret(model_max=model_max, root_path=folder_name, seeds=seeds, methods=methods, method_names=method_names,
+                           batch_size=b)
