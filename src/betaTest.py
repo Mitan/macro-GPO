2cd755807fbea07aa24e65f278bd1e55f15ef40f
Mitan <dmitrii@comp.nus.edu.sg>
2017-02-13 20:06:08 +0800

    betaTests

--- src/betaTest.py
+++ src/betaTest.py
@@ -6,9 +6,10 @@ from TestScenario import TestScenario, TestScenario_Beta
 
 if __name__ == '__main__':
 
-    h = 2
+    h = 3
     # beta_list = [0.0, 0.1, 1.0, 2.0, 5.0, 10.0, 50.0, 100.0, 200.0]
     beta_list = [0.0, 0.01, 0.05,0.1, 0.5, 1.0, 2.0, 5.0, 10.0, 50.0]
+    beta_list = [0.0, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0]
 
     args = sys.argv
     seed_0 = int(args[1])
@@ -23,7 +24,7 @@ if __name__ == '__main__':
 
     my_save_folder_root = "../testsRoadBeta2_upd/b" + str(batch_size) + "/" + str(time_slot) + "/"
 
-    for seed in range(seed_0, seed_0 + 4):
+    for seed in range(seed_0, seed_0 + 2):
         TestScenario_Beta(my_save_folder_root=my_save_folder_root, test_horizon=h, seed=seed, time_steps=t,
                           num_samples=num_samples, batch_size=batch_size, filename=filename, time_slot=time_slot,
                           beta_list=beta_list)
