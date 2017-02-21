55bc43a442cb2a5d656699cb65a4a6ecbda37ff1
Dmitrii <dmitrykharkovsky@gmail.com>
2017-02-21 11:37:24 +0800

    add BUCB-PE template

--- src/TreePlan.py
+++ src/TreePlan.py
@@ -210,6 +210,41 @@ class TreePlan:
 
         return best_expected_improv, best_action, len(next_states)
 
+    def BUCB_PE(self, x_0):
+        # x_0 stores a 2D np array of k points with history
+        max_measurement = max(x_0.history.measurements)
+
+        best_action = None
+        best_expected_improv = -1.0
+
+        # valid_actions = self.GetValidActionSet(x_0.physical_state)
+        # next_states = [self.TransitionP(x_0, a) for a in valid_actions]
+
+        next_states = self.GetNextAugmentedStates(x_0)
+        if not next_states:
+            raise Exception("qEI could not move from   location " + str(x_0.physical_state))
+
+        chol = self.gp.Cholesky(x_0.history.locations)
+
+        for x_next in next_states:
+            # x_next = self.TransitionP(x_0, a)
+
+            Sigma = self.gp.GPVariance(locations=x_0.history.locations, current_location=x_next.physical_state,
+                                       cholesky=chol)
+            weights = self.gp.GPWeights(locations=x_0.history.locations, current_location=x_next.physical_state,
+                                        cholesky=chol)
+            mu = self.gp.GPMean(measurements=x_next.history.measurements, weights=weights)
+
+            # expectedImprov = qEI(Sigma, eps, mu, max_measurement, self.batch_size)
+            expectedImprov = 0
+
+            # comparison
+            if expectedImprov >= best_expected_improv:
+                best_expected_improv = expectedImprov
+                best_action = x_next
+
+        return best_expected_improv, best_action, len(next_states)
+
     def StochasticFull(self, x_0, H):
         # by default physical state length is self.batch_size
         # but for the first step it is equal to 1, since it is just agent's position
