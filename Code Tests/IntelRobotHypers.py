d4cbb44cb92dfab7935ec46360d08dfdde48006b
Mitan <dmitrii@comp.nus.edu.sg>
2017-05-12 21:28:09 +0800

    dataset processing

---
+++ Code Tests/IntelRobotHypers.py
@@ -0,0 +1,45 @@
+import math
+import numpy as np
+import scipy
+
+from src.GaussianProcess import SquareExponential, GaussianProcess
+
+def LeaveOneOut(slot_number):
+    error = 0
+    input_data_file = '../robot/slots/slot_' + str(slot_number) + '.txt'
+    hypers_file = '../robot/hypers/hypers_' + str(slot_number) + '.txt'
+    all_data = np.genfromtxt(input_data_file)
+    hypers = np.genfromtxt(hypers_file)
+    X = all_data[:, :-1]
+    Y = all_data[:, -1:]
+    num_points, point_dimension = X.shape
+    mean = hypers[0]
+    length_scale = hypers[1: -2]
+    assert length_scale.shape[0] == point_dimension
+
+    signal_variance = hypers[-2]
+    noise_variance = hypers[-1]
+
+    covariance_function = SquareExponential(length_scale, signal_variance, noise_variance)
+    gp = GaussianProcess(covariance_function=covariance_function, mean_function=mean)
+
+    for index in range(num_points):
+        train_indexes = [i for i in range(num_points) if i != index]
+        train_X = X[train_indexes, :]
+        train_Y = Y[train_indexes, :]
+        test_location = X[index: index + 1, :]
+
+        # predict
+        cholesky = gp.Cholesky(train_X)
+        weights = gp.GPWeights(locations=train_X, current_location=test_location, cholesky=cholesky)
+        mu = gp.GPMean(measurements=train_Y, weights=weights)[0, 0]
+        error += (mu - Y[index, 0]) ** 2
+    # return math.sqrt(error / num_points), scipy.stats.skew(Y)[0], np.std(Y)
+    return math.sqrt(error / num_points) / np.std(Y), scipy.stats.skew(Y)[0]
+
+if __name__ == "__main__":
+    for k in range(25):
+
+        print k, LeaveOneOut(slot_number=k)
+
+
---
+++ Code Tests/Neighbours.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+input_file_name = '../datasets/intel-robot/coordinates.txt'
+all_points = np.genfromtxt(input_file_name)
+
+neighbours_dict = []
+
+num_points, _ = all_points.shape
+
+treshhold = 8
+for i in range(num_points):
+    id = all_points[i, 0]
+    coord = all_points[i, 1:]
+    neighbours = [id]
+
+    for j in range(num_points):
+        if j == i:
+            continue
+        other_id = all_points[j, 0]
+        other_coord = all_points[j, 1:]
+        if np.linalg.norm(coord - other_coord) < treshhold:
+            neighbours.append(other_id)
+
+    neighbours_dict.append(neighbours)
+
+
+output_filename = '../datasets/intel-robot/neighbours.txt'
+output_file = open(output_filename, 'w')
+
+for id in neighbours_dict:
+    str_list = map(str, id)
+    joined_list = " ".join(str_list)
+    output_file.write(joined_list + '\n')
+
+output_file.close()
