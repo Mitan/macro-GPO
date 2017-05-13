7e5466c8af7518a813e4994a8b6e6892de746af8
Dmitrii <dmitrykharkovsky@gmail.com>
2017-05-13 15:36:19 +0800

    neighbours consistency (all edges are two-sided)

--- Code Tests/Neighbours.py
+++ Code Tests/Neighbours.py
@@ -1,37 +1,55 @@
 import numpy as np
 
 
-input_file_name = '../datasets/intel-robot/coordinates.txt'
-all_points = np.genfromtxt(input_file_name)
+def GenerateUnconstrainedNeighbours(treshhold):
+    input_file_name = '../datasets/intel-robot/coordinates.txt'
+    all_points = np.genfromtxt(input_file_name)
 
-neighbours_dict = []
+    neighbours_dict = []
 
-num_points, _ = all_points.shape
+    num_points, _ = all_points.shape
 
-treshhold = 9
-for i in range(num_points):
-    id = all_points[i, 0]
-    coord = all_points[i, 1:]
-    neighbours = [id]
+    # treshhold = 9
+    for i in range(num_points):
+        id = all_points[i, 0]
+        coord = all_points[i, 1:]
+        neighbours = [id]
 
-    for j in range(num_points):
-        if j == i:
-            continue
-        other_id = all_points[j, 0]
-        other_coord = all_points[j, 1:]
-        if np.linalg.norm(coord - other_coord) < treshhold:
-            neighbours.append(other_id)
-
-
-    neighbours_dict.append(neighbours)
+        for j in range(num_points):
+            if j == i:
+                continue
+            other_id = all_points[j, 0]
+            other_coord = all_points[j, 1:]
+            if np.linalg.norm(coord - other_coord) < treshhold:
+                neighbours.append(other_id)
 
 
-output_filename = '../datasets/intel-robot/neighbours_raw.txt'
-output_file = open(output_filename, 'w')
+        neighbours_dict.append(neighbours)
 
-for id in neighbours_dict:
-    str_list = map(str, id)
-    joined_list = " ".join(str_list)
-    output_file.write(joined_list + '\n')
 
-output_file.close()
+    output_filename = '../datasets/intel-robot/neighbours_raw.txt'
+    output_file = open(output_filename, 'w')
+
+    for id in neighbours_dict:
+        str_list = map(str, id)
+        joined_list = " ".join(str_list)
+        output_file.write(joined_list + '\n')
+
+    output_file.close()
+
+# nodes are indexed from 1
+def CheckNeighboursConsistency():
+    input_file = 'neighbours.txt'
+    all_lines = open(input_file).readlines()
+    int_all_lines = map(lambda l: map(float, l.split()), all_lines)
+    for l in int_all_lines:
+        id = l[0]
+        neighbours = l[1:]
+        for n_id in neighbours:
+            # nodes are indexed from 1
+            n_neighbours = int_all_lines[int(n_id)-1]
+            assert n_neighbours[0] == n_id
+            assert id in n_neighbours[1:], "%r == %r" % (id, n_id)
+
+CheckNeighboursConsistency()
+# GenerateUnconstrainedNeighbours(9)
