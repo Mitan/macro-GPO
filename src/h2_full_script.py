04a6afe17c33f9ba25ea01e33613e0b1ec6bd5a6
Dmitrii <dmitrykharkovsky@gmail.com>
2018-01-10 22:57:46 +0800

    h2 full adjust

--- src/h2_full_script.py
+++ src/h2_full_script.py
@@ -6,23 +6,17 @@ if __name__ == '__main__':
 
     args = sys.argv
 
-    # seeds = map(int, args[1:])
     seed_0 = int(args[1])
-
-    # time_slot = int(args[2])
 
     # note hardcoded
     time_slot = 18
-    t, batch_size, num_samples = (4, 5, 250)
+    t, batch_size, num_samples = (4, 5, 300)
 
     filename = '../datasets/slot' + str(time_slot) + '/tlog' + str(time_slot) + '.dom'
 
-    my_save_folder_root = "../testsRoad2/b" + str(batch_size) + "/" + str(time_slot) + "/"
-    my_save_folder_root = "../testsRoad_4/"
-    my_save_folder_root = "../testsRoad_4/"
-    my_save_folder_root = "../21testsfull/"
+    my_save_folder_root = "../road_tests/21testsfull/"
 
-    for seed in range(seed_0, seed_0 + 1):
+    for seed in range(seed_0, seed_0 + 7):
         # for seed in seeds:
         TestScenario_2Full(my_save_folder_root=my_save_folder_root, seed=seed, time_steps=t, num_samples=num_samples,
                            batch_size=batch_size, filename=filename, time_slot=time_slot)
