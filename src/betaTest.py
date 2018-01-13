9ef6f1a34a64d5de246e442887a6b20d38911bf7
Dmitrii <dmitrykharkovsky@gmail.com>
2018-01-13 20:01:05 +0800

    minor

--- src/betaTest.py
+++ src/betaTest.py
@@ -13,12 +13,13 @@ if __name__ == '__main__':
 
     t, batch_size, num_samples = (4, 5, 300)
 
-    my_save_folder_root = "../noise_robot_tests/beta" + str(h) + "/"
+    my_save_folder_root = "../robot_tests/beta" + str(h) + "/"
+
     data_file = '../datasets/robot/selected_slots/slot_' + str(time_slot) + '/noise_final_slot_' + str(time_slot) + '.txt'
     neighbours_file = '../datasets/robot/all_neighbours.txt'
     coords_file = '../datasets/robot/all_coords.txt'
 
-    for seed in range(seed_0, seed_0 + 1):
+    for seed in range(seed_0, seed_0 + 3):
         TestScenario_Beta(my_save_folder_root=my_save_folder_root, test_horizon=h, seed=seed, time_steps=t,
                           num_samples=num_samples, batch_size=batch_size, time_slot=time_slot,
                           beta=beta, coords_filename=coords_file,
