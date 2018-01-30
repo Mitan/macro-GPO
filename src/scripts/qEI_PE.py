d190187d5bd1345b39d38bfec430a69f93e05070
Dmitrii <dmitrykharkovsky@gmail.com>
2018-01-30 20:20:14 +0800

    scripts

--- src/scripts/qEI_PE.py
+++ src/scripts/qEI_PE.py
@@ -3,7 +3,7 @@ from src.TestScenario import TestScenario_PE_qEI_BUCB
 if __name__ == '__main__':
 
     seeds = range(35)
-    seeds = range(42)
+    # seeds = range(42)
 
     # note hardcoded
     time_slot = 18
@@ -11,9 +11,9 @@ if __name__ == '__main__':
 
     filename = '../../datasets/slot' + str(time_slot) + '/tlog' + str(time_slot) + '.dom'
 
-    my_save_folder_root = '../../releaseTests/road/b5-18-log/'
-    my_save_folder_root = '../../new_road_tests/new_all/'
-    my_save_folder_root = '../../new_road_tests/new_all_3/'
+    my_save_folder_root = '../../releaseTests/updated_release/road/b5-18-log/'
+    #my_save_folder_root = '../../new_road_tests/new_all/'
+    # my_save_folder_root = '../../new_road_tests/new_all_3/'
 
     for seed in seeds:
         print seed
