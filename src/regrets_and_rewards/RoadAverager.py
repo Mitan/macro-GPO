eb9bbba70804cbe0bd3f5bbb1d4a4e9a64854959
Dmitrii <dmitrykharkovsky@gmail.com>
2018-10-11 21:13:10 +0800

    road averager

--- src/regrets_and_rewards/RoadAverager.py
+++ src/regrets_and_rewards/RoadAverager.py
@@ -11,7 +11,6 @@ def CalculateMetrics(metric_type,
                      plotting_type,
                      filename,
                      plot_bars):
-
     batch_size = 5
     total_budget = 20
 
@@ -56,7 +55,7 @@ def CalculateMetrics(metric_type,
     h4 = results[0]
     h1 = results[3]
     mle = results[4]
-    print "H4 -  H1 %f sigma " % round(abs((h1[1][-1] - h4[1][-1])) / sigma,4)
+    print "H4 -  H1 %f sigma " % round(abs((h1[1][-1] - h4[1][-1])) / sigma, 4)
     print "H4  -  MLE %f sigma" % round(abs((mle[1][-1] - h4[1][-1])) / sigma, 4)
 
 
@@ -69,7 +68,7 @@ def GetRoadTotalRewards():
 
 def GetRoadTotalRegrets():
     CalculateMetrics(metric_type=MetricsEnum.SimpleRegret,
-                     plotting_type = PlottingEnum.SimpleRegret,
+                     plotting_type=PlottingEnum.SimpleRegret,
                      filename='road_simple_regrets.eps',
                      plot_bars=False)
 
@@ -117,7 +116,7 @@ def GetRoadBeta2Rewards():
     CalculateMetricsBeta(h=2,
                          metric_type=MetricsEnum.AverageTotalReward,
                          plotting_type=PlottingEnum.AverageRewardBeta,
-                         input_folder= '../../releaseTests/updated_release/road/new_new_new_beta2_c/',
+                         input_folder='../../releaseTests/updated_release/road/new_new_new_beta2_c/',
                          filename='road_beta2_rewards.eps',
                          plot_bars=False)
 
@@ -126,86 +125,70 @@ def GetRoadBeta3Rewards():
     CalculateMetricsBeta(h=3,
                          metric_type=MetricsEnum.AverageTotalReward,
                          plotting_type=PlottingEnum.AverageRewardBeta,
-                         input_folder= '../../releaseTests/updated_release/road/new_beta3_c/',
+                         input_folder='../../releaseTests/updated_release/road/new_beta3_c/',
                          filename='road_beta3_rewards.eps',
                          plot_bars=False)
 
 
+def CalculateMetricsFull(metric_type,
+                         plotting_type,
+                         filename,
+                         plot_bars):
+    batch_size = 5
+    total_budget = 20
 
+    seeds = range(0, 35)
 
+    root_path = '../../releaseTests/road/tests2full-r/'
 
+    methods = ['anytime_h4', 'anytime_h2_full', 'anytime_h2', 'ei']
 
+    method_names = [r'Anytime $\epsilon$-Macro-GPO  $H = 4$  ($20$)',
+                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$ (all)',
+                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$  ($20$)',
+                    'EI (all)']
 
+    output_file = '../../result_graphs/eps/road/' + filename
 
+    result_calculator = ResultCalculator(dataset_type=DatasetEnum.Road,
+                                         root_path=root_path,
+                                         time_slot=18,
+                                         seeds=seeds,
+                                         total_budget=total_budget)
+    results = result_calculator.calculate_results(batch_size=batch_size,
+                                                  methods=methods,
+                                                  method_names=method_names,
+                                                  metric_type=metric_type)
+
+    results_plotter = ResultGraphPlotter(dataset_type=DatasetEnum.Road,
+                                         plotting_type=plotting_type,
+                                         batch_size=batch_size,
+                                         total_budget=total_budget)
+    results_plotter.plot_results(results=results, output_file_name=output_file, plot_bars=plot_bars)
+
+    for result in results:
+        print result[0], round(result[1][-1], 4), '+-', round(result[2][-1], 4)
+
+    sigma = math.sqrt(0.7486)
+    h4 = results[0]
+    h2_all = results[1]
+    h2 = results[2]
+    print "H4 -  H2 all %f sigma " % round(abs((h4[1][-1] - h2_all[1][-1])) / sigma, 4)
+    print "H2 all  -  H2 %f sigma" % round(abs((h2_all[1][-1] - h2[1][-1])) / sigma, 4)
 
 
 def GetRoad_H2Full_TotalRewards():
-    seeds = range(35)
-    batch_size = 5
-
-    # methods = ['anytime_h2_full_2121', 'anytime_h2', 'anytime_h4', 'ei']
-    methods = ['anytime_h4', 'anytime_h2_full', 'anytime_h2', 'ei']
-
-    method_names = [r'Anytime $\epsilon$-Macro-GPO  $H = 4$  ($20$)',
-                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$ (all)',
-                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$  ($20$)',
-                    'EI (all)']
-
-    root_path = '../../releaseTests/road/tests2full/'
-    root_path = '../../releaseTests/road/tests2full-r/'
-
-    output_file = '../../result_graphs/eps/road/road_h2_full_total_rewards.eps'
-
-    results = RoadRewards(batch_size=batch_size, tests_source_path=root_path, methods=methods,
-                          method_names=method_names,
-                          seeds=seeds, output_filename=output_file, plottingType=MetricsEnum.TotalReward)
-    # print results
-    h4 = results[0]
-    h2_all = results[1]
-    h2 = results[2]
-    # print h4, h2_all, h2
-    print "Rewards H4 / H2 all %f" % (h4[1][-1] / h2_all[1][-1])
-    print "Rewards H2  / H2 all %f" % (1 - h2[1][-1] / h2_all[1][-1])
+    CalculateMetricsFull(metric_type=MetricsEnum.AverageTotalReward,
+                         plotting_type=PlottingEnum.AverageTotalReward,
+                         filename='road_h2_full_total_rewards.eps',
+                         plot_bars=False)
 
 
 def GetRoadTotalRegrets_H2Full():
-    seeds = range(35)
-    batch_size = 5
-    time_slot = 18
-
-    methods = ['anytime_h4', 'anytime_h2_full', 'anytime_h2', 'ei']
-
-    method_names = [r'Anytime $\epsilon$-Macro-GPO  $H = 4$  ($20$)',
-                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$ (all)',
-                    r'Anytime $\epsilon$-Macro-GPO  $H = 2$  ($20$)',
-                    'EI (all)']
-
-    output_file = '../../result_graphs/eps/road/road_h2_full_simple_regrets.eps'
-    root_path = '../../releaseTests/road/tests2full-r/'
-    # root_path = '../../releaseTests/road/tests2full-NEW/'
-
-    regret_calculator = ResultCalculator(dataset_type=DatasetEnum.Road,
-                                         root_path=root_path,
-                                         time_slot=time_slot,
-                                         seeds=seeds)
-    results = regret_calculator.calculate_results(batch_size=batch_size,
-                                                  methods=methods,
-                                                  method_names=method_names)
-    PlotData(results=results, output_file_name=output_file,
-             plottingType=MetricsEnum.SimpleRegret, dataset=DatasetEnum.Road, plot_bars=True)
-    """
-    h4 = results[0]
-    h2_all = results[1]
-    h2 = results[2]
-    # print h4, h2_all, h2
-    sigma = math.sqrt(0.7486)
-
-    print "Regrets H4 -  H2 all %f sigma " % ((h2_all[1][-1] - h4[1][-1]) / sigma)
-    print "Regrets H2 all  -  H2 %f sigma" % ((h2[1][-1] - h2_all[1][-1]) / sigma)
-    """
-
-
-
+    CalculateMetricsFull(metric_type=MetricsEnum.SimpleRegret,
+                         plotting_type=PlottingEnum.SimpleRegret,
+                         filename='road_h2_full_simple_regrets.eps',
+                         plot_bars=False)
 
 
 
@@ -321,10 +304,10 @@ def GetRoadBeta3Regrets():
 
 if __name__ == "__main__":
     """
-    GetRoadTotalRegrets()
-    GetRoadTotalRegrets_H2Full()
-    """
     GetRoadTotalRewards()
     GetRoadTotalRegrets()
     GetRoadBeta2Rewards()
     GetRoadBeta3Rewards()
+    GetRoadTotalRegrets_H2Full()
+    """
+    GetRoad_H2Full_TotalRewards()
