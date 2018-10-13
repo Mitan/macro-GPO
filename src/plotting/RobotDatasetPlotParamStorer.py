4862fb64ca12f45ba4d71f2d17654d76e448bfec
Dmitrii <dmitrykharkovsky@gmail.com>
2018-10-13 18:13:04 +0800

    minor

--- src/plotting/RobotDatasetPlotParamStorer.py
+++ src/plotting/RobotDatasetPlotParamStorer.py
@@ -21,7 +21,7 @@ class RobotDatasetPlotParamStorer:
         elif plotting_type == PlottingEnum.AverageRewardFull:
             self.y_label_caption = "Average normalized output measurements observed by mobile robot"
             self.y_ticks_range = np.arange(0.0, 0.65, 0.05)
-            self.y_lim_range = [0.15, 0.65]
+            self.y_lim_range = [0.17, 0.65]
             self.legend_loc = 2
 
         elif plotting_type == PlottingEnum.AverageRewardBeta:
