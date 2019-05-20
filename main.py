c14972d4e97c4d1378b7c7f9b5dec9160de7cd5f
Dmitrii <dmitrykharkovsky@gmail.com>
2019-05-20 23:53:59 +0800

    added method descriptor

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 from src.enum.DatasetEnum import DatasetEnum
 from src.enum.DatasetModeEnum import DatasetModeEnum
 from src.enum.MethodEnum import Methods
-from src.run import run
+from src.run.MethodDescriptor import MethodDescriptor
+from src.run.Run import run
 
 if __name__ == '__main__':
-
-    # list of planning horizon values
-    h = 2
 
     # batch size
     batch_size = 4
 
     # total budget of function evaluations
     total_budget = 20
-
-    # number of stochastic samples generated for every node
-    num_samples = 2
-
-    # exploration parameter beta
-    beta = 0.0
 
     # seeds for evaluation
     seeds = range(3)
@@ -32,24 +24,23 @@ if __name__ == '__main__':
     # load or generate dataset. If "Load" is selected, the root dataset folder should be specified
     dataset_mode = DatasetModeEnum.Load
 
-    # execute exact or anytime algorithm
-    method = Methods.Exact
-
     # the folder containing the dataset
     # if None, the current seed folder will be used (e.g. for loading the simulated dataset)
     dataset_root_folder = './new_datasets/robot/'
 
     results_save_root_folder = './new_tests/robot/'
 
-    run(h=h,
-        batch_size=batch_size,
+    # list of methods to run
+    methods = [MethodDescriptor(method_type=Methods.Exact,
+                                h=2,
+                                beta=0.0,
+                                num_samples=2)]
+    run(batch_size=batch_size,
         total_budget=total_budget,
-        num_samples=num_samples,
-        beta=beta,
         seeds=seeds,
         dataset_type=dataset_type,
         dataset_mode=dataset_mode,
         dataset_root_folder=dataset_root_folder,
         results_save_root_folder=results_save_root_folder,
         ma_threshold=ma_threshold,
-        method=method)
+        methods=methods)
---
+++ src/run/MethodDescriptor.py
@@ -0,0 +1,25 @@
+from src.enum.MethodEnum import Methods
+
+
+class MethodDescriptor:
+    def __init__(self, method_type, h, num_samples, beta):
+
+        # exploration parameter beta for mutual information
+        self.beta = beta
+
+        # number of stochastic samples generated for every node. N in the notations of the paper
+        self.num_samples = num_samples
+
+        # planning horizon value. H in the notations of the paper
+        self.h = h
+
+        if method_type == Methods.Exact:
+            method_string = 'exact'
+        elif method_type == Methods.Anytime:
+            method_string = 'anytime'
+        else:
+            raise Exception("Method not supported")
+        # currently supported exact or anytime algorithm
+        self.method_type = method_type
+        # handle to name the folder for the execute method
+        self.method_folder_name = "{}_h{}_beta{}/".format(method_string, self.h, self.beta)
