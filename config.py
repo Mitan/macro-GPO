67f9035d1415116f857414e985459a3d07586227
Dmitrii <dmitrykharkovsky@gmail.com>
2019-05-21 18:48:01 +0800

    add config

---
+++ config.py
@@ -0,0 +1,43 @@
+from src.enum.DatasetEnum import DatasetEnum
+from src.enum.DatasetModeEnum import DatasetModeEnum
+from src.enum.MethodEnum import Methods
+from src.run.MethodDescriptor import MethodDescriptor
+
+
+class Config:
+
+
+    # batch size
+    BATCH_SIZE = 4
+
+    # type of dataset - currently Simulated, Road and Robot are supported
+    DATASET_TYPE = DatasetEnum.Simulated
+
+    # load or generate dataset. If "Load" is selected, the root dataset folder should be specified
+    DATASET_MODE = DatasetModeEnum.Load
+
+    # the folder containing the dataset
+    # if None, the current seed folder will be used (e.g. for loading the simulated dataset)
+    DATASET_ROOT_FOLDER = './new_datasets/robot/'
+
+    RESULTS_SAVE_ROOT_FOLDER = './new_tests/simulated/'
+
+    # total budget of function evaluations
+    TOTAL_BUDGET = 20
+
+    # seeds for evaluation
+    SEEDS = range(3)
+
+    # threshold for selecting the number of random macro-actions. Selected when
+    MA_THRESHOLD = 20
+
+    # list of methods to run
+    METHODS = [MethodDescriptor(method_type=Methods.Exact,
+                                h=2,
+                                beta=120.0,
+                                num_samples=2),
+               MethodDescriptor(method_type=Methods.Exact,
+                                h=1,
+                                beta=11.0,
+                                num_samples=2)
+               ]
--- main.py
+++ main.py
@@ -1,58 +1,20 @@
-from src.enum.DatasetEnum import DatasetEnum
-from src.enum.DatasetModeEnum import DatasetModeEnum
-from src.enum.MethodEnum import Methods
-from src.run.MethodDescriptor import MethodDescriptor
+from config import Config
 from src.run.MethodRunner import MethodRunner
 
 if __name__ == '__main__':
-    # batch size
-    batch_size = 4
 
-    # type of dataset - currently Simulated, Road and Robot are supported
-    dataset_type = DatasetEnum.Simulated
-
-    # load or generate dataset. If "Load" is selected, the root dataset folder should be specified
-    dataset_mode = DatasetModeEnum.Load
-
-    # the folder containing the dataset
-    # if None, the current seed folder will be used (e.g. for loading the simulated dataset)
-    dataset_root_folder = './new_datasets/robot/'
-    dataset_root_folder = None
-
-    method_runner = MethodRunner(dataset_type=dataset_type,
-                                 dataset_mode=dataset_mode,
-                                 batch_size=batch_size,
-                                 dataset_root_folder=dataset_root_folder)
-
-    results_save_root_folder = './new_tests/simulated/'
-
-    # total budget of function evaluations
-    total_budget = 20
-
-    # seeds for evaluation
-    seeds = range(3)
-
-    # threshold for selecting the number of random macro-actions. Selected when
-    ma_threshold = 20
-
-    # list of methods to run
-    methods = [MethodDescriptor(method_type=Methods.Exact,
-                                h=2,
-                                beta=20.0,
-                                num_samples=2),
-               MethodDescriptor(method_type=Methods.Exact,
-                                h=1,
-                                beta=1.0,
-                                num_samples=2)
-               ]
-    #
-    # method_runner.run(total_budget=total_budget,
-    #                   seeds=seeds,
-    #                   results_save_root_folder=results_save_root_folder,
-    #                   ma_threshold=ma_threshold,
-    #                   methods=methods)
-
-    method_runner.calculate_results(methods=methods,
-                                    seeds=seeds,
-                                    total_budget=total_budget,
-                                    results_save_root_folder=results_save_root_folder)
+    method_runner = MethodRunner(dataset_type=Config.DATASET_TYPE,
+                                 dataset_mode=Config.DATASET_MODE,
+                                 batch_size=Config.BATCH_SIZE,
+                                 dataset_root_folder=Config.DATASET_ROOT_FOLDER)
+    """
+    method_runner.run(total_budget=Config.TOTAL_BUDGET,
+                      seeds=Config.SEEDS,
+                      results_save_root_folder=Config.RESULTS_SAVE_ROOT_FOLDER,
+                      ma_threshold=Config.MA_THRESHOLD,
+                      methods=Config.METHODS)
+    """
+    method_runner.calculate_results(methods=Config.METHODS,
+                                    seeds=Config.SEEDS,
+                                    total_budget=Config.TOTAL_BUDGET,
+                                    results_save_root_folder=Config.RESULTS_SAVE_ROOT_FOLDER)
